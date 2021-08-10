3065e5a3cfa2790b5ca7088b993d8b55a26a6ff8
Weggins <Emmanuelmensah735@gmail.com>
2021-08-10 20:44:20 +0000

    updated index.js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from 'react-redux';
+import Store from './Store/Store'
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={Store}>
+    <React.StrictMode>
     <App />
   </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
