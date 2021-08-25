0717ba35a7826c1b20015a27d5c9b21a7be8feb8
Weggins <Emmanuelmensah735@gmail.com>
2021-08-25 02:23:26 +0000

    updated Actions

--- src/Actions/ContactActions.js
+++ src/Actions/ContactActions.js
@@ -7,3 +7,16 @@ export const addContact = (contact) => {
         payload: contact
     }
 }
+export const editContact = (updatedContact) => {
+    return {
+        type: "EDIT_CONTACT",
+        payload: updatedContact
+    }
+}
+
+export const deleteContact = (contact_id) => {
+    return {
+        type: "DELETE_CONTACT",
+        payload: contact_id
+    }
+}
