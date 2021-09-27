13d52d642f7d2e4e02618a0e3ca435d9b2efe097
Weggins <Emmanuelmensah735@gmail.com>
2021-09-27 12:01:05 +0000

    updated reducer

--- src/Reducers/ContactReducer.js
+++ src/Reducers/ContactReducer.js
@@ -1,34 +1,39 @@
+import { ADD_CONTACT, SET_ALL_CONTACTS } from "../Actions/ContactActions";
 const initialState = {
     contacts: [
-         {
-           Name: 'John',
-           Phone: '02454576',
-           Location: 'Kumasi',
-           id : '3456iutre6u'
-         }
+        //  {
+        //    Name: 'John',
+        //    Phone: '02454576',
+        //    Location: 'Kumasi',
+        //    id : '3456iutre6u'
+        //  }
       ],
     } 
 
     const contactReducer = (state = initialState, action) => {
         switch (action.type) {
-            case "ADD_CONTACT":
+            case ADD_CONTACT:
                 return { ...state.contacts, contacts: [...state.contacts, action.payload] };
     
             case "EDIT_CONTACT":
-             const newContacts = state.contacts.map((contact) =>
+          return{
+            ...state, contacts: state.contacts.map((contact) =>
             contact.id === action.payload.id ? action.payload : contact
           )
-    
-            return {...state, contacts: newContacts};
+          }
     
           case "DELETE_CONTACT":
             const contactSaved = state.contacts.filter((contact) => {
               return contact.id !== action.payload;
-            }); 
-            return {...state, contacts: contactSaved};
-            default:
-                return state;
+            });
+            return{...state, contacts: contactSaved}
+
+            case SET_ALL_CONTACTS:
+            return{...state, contacts: action.payload}
+
+          default:
+            return state;
         }
-    }
+    };
     
-    export default contactReducer
+    export default contactReducer;
